2e121a8c6962284e7c0b28e06892dde366eba7f4
wcolts2000 <wcolts2000@gmail.com>
2019-07-16 13:34:26 -0700

    merge_sort complete, starting stretch problems

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -9,42 +9,42 @@ def merge(arrA, arrB):
     while i < len(arrA) and j < len(arrB):
         if arrB[j] > arrA[i]:
             merged_arr.append(arrA[i])
-            i+=1
+            i += 1
         else:
             merged_arr.append(arrB[j])
-            j+=1
+            j += 1
     while i < len(arrA):
         merged_arr.append(arrA[i])
-        i+=1
+        i += 1
     while j < len(arrB):
         merged_arr.append(arrB[j])
-        j+=1
+        j += 1
     return merged_arr
 
-arr1 = [1,3,5,7]
-arr2 = [2,6,9,10]
-arr3 = merge(arr1, arr2)
-print(arr3)
+
+# arr1 = [1,3,5,7]
+# arr2 = [2,6,9,10]
+# arr3 = merge(arr1, arr2)
+# print(arr3)
 # TO-DO: implement the Merge Sort function below USING RECURSION
+
+
 def merge_sort(arr):
         # TO-DO
-    # midpoint = len(arr) // 2
-    # left = []
-    # right = []
-    # if len(arr) > 1:
-    #     left = arr[:midpoint]
-    #     right = arr[midpoint:]
-    #     merge_sort(left)
-    #     merge_sort(right)
+    if len(arr) <= 1:
+        return arr
+    midpoint = len(arr) // 2
+    left = merge_sort(arr[:midpoint])
+    right = merge_sort(arr[midpoint:])
 
-    # merge(left, right)
-
-    return arr
+    # merge_sort(left)
+    # merge_sort(right)
+    return merge(left, right)
 
 
 arr = [1, 9, 5, 3]
-# merge_sort(arr)
-# print(arr)
+arr2 = merge_sort(arr)
+print(arr2)
 # STRETCH: implement an in-place merge sort algorithm
 
 
